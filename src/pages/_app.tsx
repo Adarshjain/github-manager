9487ae789ada282ec8f7b2c62714b1e5fa545f0b
Adarsh Jain <adarshjain9628@gmail.com>
2023-02-08 23:58:25 +0530

    basic git ogin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { SessionProvider } from "next-auth/react"
 
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({
+    Component,
+    pageProps: { session, ...pageProps }
+}) => {
+  console.log("NEXTAUTH_URL", process.env.NEXTAUTH_URL)
+
+  return <SessionProvider session={session}>
+               <Component {...pageProps} />
+             </SessionProvider>;
 };
 
 export default MyApp;
